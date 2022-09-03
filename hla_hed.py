84ebfc83b3c148de2f6460edc11a7e9efb26062d
sunhuaibo <sunhuaibo@gmail.com>
2022-09-03 10:20:35 +0800

    Update

--- hla_hed.py
+++ hla_hed.py
@@ -8,13 +8,14 @@
 # =====================================
 
 import os
+from typing import Dict, List, NoReturn
 import pandas as pd
 from Bio import SeqIO
 from pathlib import Path
 from itertools import combinations
-from argparse import ArgumentParser, RawDescriptionHelpFormatter
+from argparse import ArgumentParser, Namespace, RawDescriptionHelpFormatter
 
-def get_opt():
+def get_opt() -> Namespace:
     """
     Input HLA file format
     
@@ -22,7 +23,7 @@ def get_opt():
     p1     A*01:01 A*01:03 B*07:01 B*07:02 C*01:01 C*01:02
     p2     A*01:01 A*01:03 B*07:01 B*07:02 C*01:01 C*01:02
     
-    If you use this tool, please cite the following three papers.
+    If you use this tool, please cite the three following papers.
     
     Grantham R. Amino acid difference formula to help explain protein evolution. Science. 1974 Sep 6;185(4154):862-4. doi: 10.1126/science.185.4154.862. PMID: 4843792.
     Pierini F, Lenz TL. Divergent Allele Advantage at Human MHC Genes: Signatures of Past and Ongoing Selection. Mol Biol Evol. 2018 Sep 1;35(9):2145-2158. doi: 10.1093/molbev/msy116. PMID: 29893875; PMCID: PMC6106954.
@@ -39,32 +40,32 @@ def get_opt():
     parser.add_argument("-o", required=True, help="Output file name.")
 
     parse = parser.parse_args()
-    return(parse)
+    return parse
 
-def check_file(infile):
+def check_file(infile: str) -> NoReturn:
     if not infile.exists:
-        raise Exception(f"{str(infile)} file is not exist")
+        raise Exception(f"{str(infile)} file does not exist")
 
-def read_fasta(infile):
+def read_fasta(infile: str) -> dict:
     infile = Path(infile)
     check_file(infile)
     record = SeqIO.parse(infile, "fasta")
-    seq_array = {seq.id: str(seq.seq) for seq in record}
-    seq_len = [len(value) for value in seq_array.values()]
+    sequences = {seq.id: str(seq.seq) for seq in record}
+    seq_len = [len(value) for value in sequences.values()]
     if len(set(seq_len)) != 1:
         raise Exception("Input sequences length is not equality")
-    return(seq_array)
+    return sequences
 
-def read_aa(infile):
+def read_aa(infile: str) -> dict:
     infile = Path(infile)
     check_file(infile)
     df = pd.read_csv(infile, header=0, sep="\t", index_col=0)
     aa_pairwise_dis = df.to_dict()
-    return(aa_pairwise_dis)
+    return aa_pairwise_dis
 
-def calculate_distange(hla1, hla2, sequence, distance):
-    seq_hla1 = sequence.get(hla1, False)
-    seq_hla2 = sequence.get(hla2, False)
+def calculate_distange(hla1: str, hla2: str, sequences: dict, distance: dict) -> float:
+    seq_hla1 = sequences.get(hla1, False)
+    seq_hla2 = sequences.get(hla2, False)
     if not seq_hla1 or not seq_hla2:
         return("NA")
     else:
@@ -75,12 +76,12 @@ def calculate_distange(hla1, hla2, sequence, distance):
             aa2 = seq_hla2[i]
             dis += distance[aa1][aa2]
         dis = dis / seq_len
-        return(dis)
+        return dis
 
 
 def main():
     opt = get_opt()
-    seq_array = read_fasta(opt.f)
+    sequences = read_fasta(opt.f)
     aa_pairwise_dis = read_aa(opt.d)
 
     infile = Path(opt.i)
@@ -88,7 +89,6 @@ def main():
     check_file(infile)
 
     df = pd.read_csv(infile, header=0, sep="\t")
-    
 
     if opt.p:
         df2 = pd.melt(df, id_vars=["Sample"], value_vars=["A1", "A2", "B1","B2", "C1","C2"])
@@ -99,7 +99,7 @@ def main():
         with open(outfile, "w") as fw:
             fw.write("\t".join(outheader) + "\n")
             for allele1, allele2 in alleles_pair:
-                dis_hla_pair = calculate_distange(allele1, allele2, seq_array, aa_pairwise_dis)
+                dis_hla_pair = calculate_distange(allele1, allele2, sequences, aa_pairwise_dis)
                 outline = [allele1, allele2, dis_hla_pair]
                 outline = [str(x) for x in outline]
 
@@ -111,15 +111,15 @@ def main():
             for _, line in df.iterrows():
                 hla_a1 = line["A1"]
                 hla_a2 = line["A2"]
-                dis_hla_a = calculate_distange(hla_a1, hla_a2, seq_array, aa_pairwise_dis)
+                dis_hla_a = calculate_distange(hla_a1, hla_a2, sequences, aa_pairwise_dis)
 
                 hla_b1 = line["B1"]
                 hla_b2 = line["B2"]
-                dis_hla_b = calculate_distange(hla_b1, hla_b2, seq_array, aa_pairwise_dis)
+                dis_hla_b = calculate_distange(hla_b1, hla_b2, sequences, aa_pairwise_dis)
                 
                 hla_c1 = line["C1"]
                 hla_c2 = line["C2"]
-                dis_hla_c = calculate_distange(hla_c1, hla_c2, seq_array, aa_pairwise_dis)
+                dis_hla_c = calculate_distange(hla_c1, hla_c2, sequences, aa_pairwise_dis)
 
                 if dis_hla_a == "NA" or dis_hla_b == "NA" or dis_hla_c == "NA":
                     dis_mean = "NA"
